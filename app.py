f566dffd47dc5ccfc39c127f161ca778b8c6d411
Esumandi <202380248@PSU.PALAWAN.EDU.PH>
2025-12-07 22:36:42 +0800

    setup

--- app.py
+++ app.py
@@ -3,18 +3,23 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def home():  # put application's code here
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
 app.config['MYSQL_HOST'] = '127.0.0.1'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'Pentagon0211'
 app.config['MYSQL_DB'] = 'CS_ELECT'
 
 mysql = MySQL(app)
+
+@app.route('/')
+def home():
+    return 'Hello World!'
+
+@app.route("/testdb")
+def testdb():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT DATABASE()")
+    data = cur.fetchone()
+    return str(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
